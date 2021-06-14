657600806355a0e5f3bbbefaceef1503a477370c
alexoui87 <ptitalex87000@gmail.com>
2021-06-14 14:02:56 +0200

    mise en place de la liste de choix des types de véhicule

--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -6,6 +6,7 @@ use App\Entity\Client;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ClientType extends AbstractType
 {
@@ -16,9 +17,19 @@ class ClientType extends AbstractType
             ->add('prenom')
             ->add('datePremiereSaisie')
             ->add('adresse')
-            ->add('typeVehicule')
+            ->add('typeVehicule' , ChoiceType::class, [
+                'choices' => [
+                    'Véhicule ancien' => 'véhicule ancien',
+                    'SUV' => 'suv',
+                    'Cabriolet' => 'cabriolet',
+                    'Véhicule coupé' => 'véhicule coupé',
+                    'Berline' => 'berline',
+                    '4x4' => '4x4',
+                    'Non gérable' => 'non gérable',
+                ]
+            ])
             ->add('plaqueImmat')
-            ->add('isActif')
+
         ;
     }
 
