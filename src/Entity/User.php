e606f86e199f42a502a1f7f2c3bdf45787f14a2a
alexoui87 <ptitalex87000@gmail.com>
2021-06-24 17:18:29 +0200

    MAJ User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -93,7 +93,7 @@ class User implements UserInterface
     /**
      * @see UserInterface
      */
-    public function getPassword(): string
+    public function getPassword(): ?string
     {
         return $this->password;
     }
