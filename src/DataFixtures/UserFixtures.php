b9e84fd95680cb9457c385bead72f6245eb21997
alexoui87 <ptitalex87000@gmail.com>
2021-06-24 15:22:02 +0200

    MAJ connexion

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures
+    extends Fixture
+    implements OrderedFixtureInterface
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // Client administrator account
+        $user = new User();
+        $user->setLogin('piston')
+            ->setPassword($this->encoder->encodePassword($user, 'boulon'))
+            ->setRoles(['ROLE_ADMIN']);
+        $manager->persist($user);
+
+
+        // Default admin account
+        $admin = new admin();
+        $admin->setNom('Piston');
+        $admin->setPrenom('Boulon')
+
+            ->setUser($user);
+        $manager->persist($admin);
+
+        // Flush to DB
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 2;
+    }
+}
