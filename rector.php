5881b3d4eed34bf731cf76d12ce1c3fee7ea0ad7
stuntmanmike87 <mich.perigaud@gmail.com>
2023-08-04 22:11:20 +0200

    Rector & PHPStan configs + updates libs (20230804)

--- rector.php
+++ rector.php
@@ -2,13 +2,16 @@
 
 declare(strict_types=1);
 
-use Rector\CodingStyle\Rector\ClassConst\VarConstantCommentRector;
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\SetList;
-use Rector\Set\ValueObject\LevelSetList;
-//use Rector\Symfony\Rector\Class_\ChangeFileLoaderInExtensionAndKernelRector;
+use Rector\Core\Configuration\Option;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Symfony\Set\SymfonySetList;
+//use Rector\Symfony\Rector\Class_\ChangeFileLoaderInExtensionAndKernelRector;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\PHPUnit\Set\PHPUnitLevelSetList;
+use Rector\CodingStyle\Rector\ClassConst\VarConstantCommentRector;
 
 /**
  * @see \Rector\Config\RectorConfig::symfonyContainerXml()
@@ -18,15 +21,39 @@ return static function (RectorConfig $rectorConfig): void {
     //     SetList::RECTOR_CONFIG
     // ]);
 
-    $rectorConfig->paths([
-        __DIR__ . '/src',
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
+
+    //$rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
+
+    // basic rules
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses();
+
+    $rectorConfig->sets([
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        //SetList::DEAD_CODE,
+        // SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION,
+        // SetList::EARLY_RETURN,
+        // SetList::INSTANCEOF,
+        // SetList::NAMING,
+        SetList::PHP_82,
+        // SetList::PRIVATIZATION,
+        // SetList::STRICT_BOOLEANS,
+        // SetList::TYPE_DECLARATION,
+        LevelSetList::UP_TO_PHP_82,
     ]);
+
+    // symfony rules
+    /* $rectorConfig->symfonyContainerPhp(
+        __DIR__ . '/var/cache/website/dev/App_KernelDevDebugContainer.xml'
+    );
 
     $rectorConfig->symfonyContainerXml(
         __DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml'
-    );
+    ); */
 
-    $rectorConfig->import(SymfonySetList::SYMFONY_63);
+    /* $rectorConfig->import(SymfonySetList::SYMFONY_63);
 
     $parameters = $rectorConfig->parameters();
     
@@ -35,32 +62,28 @@ return static function (RectorConfig $rectorConfig): void {
     $parameters->set(
         Option::class,
         __DIR__ . '/var/cache/dev/AppKernelDevDebugContainer.xml'
-    );
+    ); */
 
     $rectorConfig->sets([
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        //SetList::DEAD_CODE,
-        SetList::PHP_82,
-        // SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION,
-        // SetList::EARLY_RETURN,
-        // SetList::INSTANCEOF,
-        // SetList::PRIVATIZATION,
-        // SetList::PSR_4,
-        // SetList::TYPE_DECLARATION,
-    ]);
-
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82
-    ]);
-
-    $rectorConfig->sets([
-        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+        //SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
         SymfonySetList::SYMFONY_63,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        //SymfonySetList::SYMFONY_STRICT,
     ]);
+
+        // doctrine rules
+        $rectorConfig->sets([
+            DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+            DoctrineSetList::DOCTRINE_CODE_QUALITY,
+        ]);
+    
+        // phpunit rules
+        $rectorConfig->sets([
+            //PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
+            PHPUnitSetList::PHPUNIT_100,
+            PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+            PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
+        ]);
 
     $rectorConfig->skip([
         VarConstantCommentRector::class,
